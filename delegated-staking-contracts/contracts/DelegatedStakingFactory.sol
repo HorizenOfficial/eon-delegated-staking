ee50239f05792ee4e6ccaff2eb6dce95da227901
NisticoAn <nistic.andrea@gmail.com>
2024-04-19 17:18:51 +0200

    feat: add factory

---
+++ delegated-staking-contracts/contracts/DelegatedStakingFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./interfaces/ForgerStakesV2.sol";
+import "./DelegatedStaking.sol";
+
+contract DelegatedStakingFactory {
+
+    ForgerStakesV2 public immutable FORGER;
+    mapping(bytes32 => mapping(bytes32 => mapping(bytes1 => address[]))) public deployedContractsFromParams;
+    //events
+    event ContractDeployed(address deployedContract, bytes32 indexed signPubKey, bytes32 indexed vrf1, bytes1 indexed vrf2);
+
+    //constructor
+    constructor(ForgerStakesV2 _forger) {
+        FORGER = _forger;
+    }
+
+    function deployDelegatedStakingReferenceImplementation(bytes32 signPubKey, bytes32 vrf1, bytes1 vrf2) public returns(DelegatedStaking) {
+        DelegatedStaking newInstance = new DelegatedStaking(signPubKey, vrf1, vrf2, FORGER);
+        deployedContractsFromParams[signPubKey][vrf1][vrf2].push(address(newInstance));
+
+        emit ContractDeployed(address(newInstance), signPubKey, vrf1, vrf2);
+        return newInstance;
+    }
+}
